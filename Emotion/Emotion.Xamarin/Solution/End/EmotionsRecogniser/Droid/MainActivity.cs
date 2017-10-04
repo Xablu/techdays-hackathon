58ae7d7fbc10d6a7573db221d732c1ceb960a0d3
Mattijs Kuhlmann <matju.k@hotmail.com>
2017-10-04 16:45:39 +0200

    Adding new file structure, Emotion.Xamarin End files
    Also added .DS_Store into gitignore

---
+++ Emotion/Emotion.Xamarin/Solution/End/EmotionsRecogniser/Droid/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Plugin.Media;
+
+namespace EmotionsRecogniser.Droid
+{
+    [Activity(Label = "EmotionsRecogniser.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override async void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            await CrossMedia.Current.Initialize();
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            LoadApplication(new App());
+        }
+    }
+}
